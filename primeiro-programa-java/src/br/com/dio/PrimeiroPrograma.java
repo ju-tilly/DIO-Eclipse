2535b7e0462728cf5dbbf281e1fff17a7d7a62b8
ju-tilly <juce.duarte.f@gmail.com>
2022-03-19 23:57:17 -0300

    First commit

---
+++ primeiro-programa-java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		gato.setNome("Borel");
+		gato.setCor("Preto e branco");
+		gato.setIdade(2);
+		Livros livros = new Livros();
+
+		System.out.println("Nome do Gato é : " + gato.getNome() + "\nCor do gato é : " + gato.getCor() + "\nIdade gato é: " + gato.getIdade());
+		System.out.println(livros);
+		/*
+		 * int a = 10; int b = 20; System.out.println("Hello Tester Eclipse! " + (a+b));
+		 */
+	}
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
